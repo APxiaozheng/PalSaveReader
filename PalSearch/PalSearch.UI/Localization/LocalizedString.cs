d32aa240c9a0a75f10be6da26858c6d71cc5dd86
APxiaozheng <APxiaozheng@users.noreply.github.com>
2026-07-07 01:50:44 +0000

    feat: 支持多语言动态切换
    新增 LocaleBindingSource（INotifyPropertyChanged），监听 Translator.LocaleChanged
    事件。LocExtension 通过 Binding 绑定到 LocaleBindingSource.CurrentLocale，
    切换语言时所有 {loc:Loc} 文本自动刷新。

--- PalSearch/PalSearch.UI/Localization/LocalizedString.cs
+++ PalSearch/PalSearch.UI/Localization/LocalizedString.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace PalSearch.UI.Localization
@@ -15,7 +18,49 @@ namespace PalSearch.UI.Localization
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return Translator.Get(Key ?? "");
+            var binding = new Binding(nameof(LocaleBindingSource.CurrentLocale))
+            {
+                Source = LocaleBindingSource.Instance,
+                Mode = BindingMode.OneWay,
+                Converter = new LocConverter(Key ?? "")
+            };
+
+            return binding.ProvideValue(serviceProvider);
         }
+
+        private class LocConverter : IValueConverter
+        {
+            private readonly string key;
+
+            public LocConverter(string key)
+            {
+                this.key = key;
+            }
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return Translator.Get(key);
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+
+    internal class LocaleBindingSource : INotifyPropertyChanged
+    {
+        public static LocaleBindingSource Instance { get; } = new();
+
+        static LocaleBindingSource()
+        {
+            Translator.LocaleChanged += () =>
+                Instance.PropertyChanged?.Invoke(Instance, new PropertyChangedEventArgs(nameof(CurrentLocale)));
+        }
+
+        public TranslationLocale CurrentLocale => Translator.CurrentLocale;
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
